14fabfae1d28e2eb3d5de5e5ad44201b3ba6b260
Maciej Siwicki <maciej.siwicki00@gmail.com>
2026-07-07 23:01:32 +0200

    add unit controller

---
+++ TacticalC2.Api/Controllers/UnitsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TacticalC2.Api.InMemory;
+using TacticalC2.Domain.Entities;
+
+namespace TacticalC2.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class UnitsController(InMemoryUnitStore store) : ControllerBase
+{
+    [HttpGet]
+    public ActionResult<IEnumerable<Unit>> GetAll()
+    {
+        return Ok(store.Units);
+    }
+}
--- TacticalC2.Api/Program.cs
+++ TacticalC2.Api/Program.cs
@@ -1,8 +1,14 @@
+using Scalar.AspNetCore;
+using TacticalC2.Api.InMemory;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddControllers();    
+
+builder.Services.AddSingleton<InMemoryUnitStore>();
 
 var app = builder.Build();
 
@@ -10,8 +16,11 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.MapScalarApiReference();
 }
 
 app.UseHttpsRedirection();
+
+app.MapControllers();
 
 app.Run();
